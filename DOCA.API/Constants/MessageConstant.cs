8cde3cf0f57c40780be2a61d677862f7188e21a9
Magic Flexing <134113309+Nguyen-Huy-Phuc@users.noreply.github.com>
2025-04-05 22:46:13 +0700

    Fix: login message

--- DOCA.API/Constants/MessageConstant.cs
+++ DOCA.API/Constants/MessageConstant.cs
@@ -5,7 +5,7 @@ public class MessageConstant
     public static class User
     {
         public const string UserNotFound = "Username không tồn tại";
-        public const string LoginFail = "Đăng nhập không thành công";
+        public const string LoginFail = "Email hoặc password không chính xác";
         public const string UserNameExisted = "Tên đăng nhập đã tồn tại";
         public const string PhoneNumberExisted = "Số điện thoại đã tồn tại";
         public const string RegisterFail = "Tạo tài khoản thất bại";
